30689ae1d51e224d91ca1af3274ff102376a1bba
Miguel Brito <miguelfbrito11@gmail.com>
2017-11-17 02:43:23 +0000

    Adição do texto dos trilhos em mouse hover, estrutura inicial para histórico (1h30)

---
+++ Trails4Health/Controllers/HistoryController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trails4Health.Controllers
+{
+    public class HistoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
