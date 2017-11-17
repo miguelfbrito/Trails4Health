7911061c6da8d7e9de4468695b95da5943ec89cc
Miguel Reis Pereira <miguelrp13@hotmail.com>
2017-11-17 10:28:54 +0000

    Signed-off-by: Miguel Reis Pereira <miguelrp13@hotmail.com> Models Stage

---
+++ Trails4Health/Models/FakeStageRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class FakeStageRepository : IStageRepository
+    {
+
+        IEnumerable<Stage> Stages => new List<Stage>
+
+       {
+
+            new Stage {idStage = 1, StageName = "Etapa adjacente a Lagoa Comprida",
+                StageStartLoc = "Lagoa Comprida", StageEndLoc = "Segunda Lagoa",
+                Distance = 10, Duration = 4 //dist kms duração horas
+            },
+            new Stage {idStage = 1, StageName = "Etapa do Sabugueiro ",
+                StageStartLoc = "Floresta", StageEndLoc = "Sabugueiro",
+                Distance = 1, Duration = 1 //dist kms duração horas
+            }
+
+
+    };
+
+    }
+}
---
+++ Trails4Health/Models/Stage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class Stage
+    {
+ 
+
+        public int idStage { get; set; }
+        public String StageName { get; set; }
+        public String StageStartLoc { get; set; }
+        public String StageEndLoc { get; set; }
+        public int Distance { get; set; }
+        public int Duration { get; set; }
+
+    }
+}
