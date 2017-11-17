a744c4b267b8fc70da3f28c43c2573b4c593d532
ruiparedes <paredesrui16@gmail.com>
2017-11-17 10:57:20 +0000

    Implementação da página Trails Management (1h:10min)
    -Criação da View
    -Criação de um Fake Repository
    -Criação de um ITrailsRepository
    
    -FALTA: Resolver problema foreach TrailsManagement

---
+++ Trails4Health/Models/FakeTrailsRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class FakeTrailsRepository : ITrailsRepository
+    {
+        //public IEnumerable<Trail> Trails => new List<Trail>
+        //{
+        //  new Trail {Name="Trilho Serra 1", AverageDuration=120, DistanceToTravel=10,Start_Loc="Manteigas",End_Loc="Serra",RecommendedTime="Summer",Is_Activated=true,ID_Difficulty=1}
+        //};
+        public IEnumerable<Trail> Trails => new List<Trail>
+        {
+            new Trail {Name="Trilho Serra 1", AverageDuration=120, DistanceToTravel=10,Start_Loc="Manteigas",End_Loc="Serra",RecommendedTime="Summer",Is_Activated=true,ID_Difficulty=1}
+        };
+    }
+}
---
+++ Trails4Health/Models/Trail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trails4Health.Models
+{
+    public class Trail
+    {
+        public int ID_Trail { get; set; }
+        public string Name { get; set; }
+        public int AverageDuration { get; set; }
+        public int DistanceToTravel { get; set; }
+        public string Start_Loc { get; set; }
+        public string End_Loc { get; set; }
+        public string RecommendedTime { get; set; }
+        public bool Is_Activated { get; set; }
+        public int ID_Difficulty {get;set;}
+    }
+}
