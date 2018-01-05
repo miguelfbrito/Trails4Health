3c83398bcf9e6fa4f14f33478cdda5f64cbc652b
Miguel Brito <miguelfbrito11@gmail.com>
2018-01-05 18:06:53 +0000

    Validações do Histórico com criação de classe customizável, alteracoes na View de resumo de dados. (2h)

--- Trails4Health/Models/Historic.cs
+++ Trails4Health/Models/Historic.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,12 +13,19 @@ namespace Trails4Health.Models
 
         public int HistoricID { get; set; }
 
+        [Range(30, 500, ErrorMessage = "Tempo limitado entre 30 e 500 minutos.")]
         public int TimeTaken { get; set; }
+
+        [StringLength(2000, MinimumLength = 10, ErrorMessage = "Tamanho limitado entre 10 e 2000 caracteres.")]
         public string Observations { get; set; }
+
+        string currentDate = DateTime.Now.ToString("dd/MM/yyyy");
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        [CheckDateRange()]
         public DateTime RealizationDate { get; set; }
 
-        
-
+  
         public Difficulty Difficulty  { get; set; }
         public int? DifficultyID { get; set; }
 
@@ -27,5 +36,25 @@ namespace Trails4Health.Models
         public Tourist Tourist { get; set; }
         public int TouristID { get; set; }
 
+
+
     }
+
+}
+
+public class CheckDateRangeAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        DateTime dataInicioPlataforma = DateTime.Parse("01/01/2017");
+        DateTime d = (DateTime)value;
+        if (d <= DateTime.Now && d >= dataInicioPlataforma )
+        {
+            ToString();
+            return ValidationResult.Success;
+        }
+
+        return new ValidationResult(ErrorMessage ?? "Data inválida");
+    }
+
 }
