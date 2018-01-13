c412593d64d7c958d97f00bbc3c9bfca45947c6d
ruiparedes <paredesrui16@gmail.com>
2018-01-13 22:58:13 +0000

    Criação da view TrailInfo para posteriormente mostrar info do trilho aos turistas. Mudança da view Details dos trilhos e do HomeController(2h e 30min)

--- Trails4Health/Controllers/HomeController.cs
+++ Trails4Health/Controllers/HomeController.cs
@@ -4,15 +4,59 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Trails4Health.Models;
+using Microsoft.EntityFrameworkCore;
+using Trails4Health.Models.ViewModels;
 
 namespace Trails4Health.Controllers
 {
     [RequireHttps]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Trails.Include(t => t.Season).Include(t => t.Slope).Include(t => t.StatusTrails);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Home/TrailInfo/5
+        public async Task<IActionResult> TrailInfo(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ViewModelTrailsDetails vmtd = new ViewModelTrailsDetails();
+
+            var trail = await _context.Trails
+                .Include(t => t.Season)
+                .Include(t => t.Slope)
+                .SingleOrDefaultAsync(m => m.TrailID == id);
+
+            vmtd.trail = trail;
+            var statustrails = await _context.StatusTrails.
+                Include(t => t.Status).
+               Where(m => m.TrailID == trail.TrailID).
+               ToListAsync();
+            vmtd.statustrails = statustrails;
+
+            if (trail == null)
+            {
+                return NotFound();
+            }
+            return View(vmtd);
         }
 
         public IActionResult About()
